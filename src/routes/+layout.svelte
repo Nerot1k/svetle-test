0764d79346610ac7a4c01076ea8ddb882de91bdd
Artem <artemtusaliev22@gmail.com>
2023-05-23 19:19:57 +0500

    d

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,54 +1,52 @@
 <nav>
-
     <div class="nav-container">
-      <a href="/" class="nav-logo" title="Back to Homepage">Артём Тушалиев</a>
-      <div class="nav-links"></div>
+        <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
+        <div class="nav-links">
+            {#each nav as link}
+				{#if link.isShow}
+				<a href={link.href} class="link">{link.title}</a>
+				{/if}
+            {/each}
+        </div>
     </div>
-  </nav>
-  {#each nav as link}
-<a href={link.href} class="link">{link.title}</a>
-{/each}
-  
-  <div class="container">
+</nav>
+
+<div class="container">
     <!-- Pages will be injected below -->
     <slot></slot>
-  </div>
-  
-  <script>
-   const nav = [
-  {
-    title: 'Обо мне ',
-    href: '/about',
-    isShow: true,
-  },
-  {
-    title: 'Блог ',
-    href: '/blog',
-    isShow: true,
-  },
-
-  {
-    title: 'Портфолио ',
-    href: '/project',
-    isShow: true,
-  },
-  {
-    title: 'Контакты ',
-    href: '/contact',
-    isShow: true,
-  },
-  {
-    title: 'temp',
-    href: '/temp',
-    isShow: true,
-  },
-];
-
+</div>
+<script>
+    	const nav = [
+		{
+			title: 'Обо мне',
+			href: '/about',
+			isShow: true
+		},
+		{
+			title: 'Блог',
+			href: '/blog',
+			isShow: true
+		},
+		{
+			title: 'Портфолио',
+			href: '/project',
+			isShow: true
+		},
+		{
+			title: 'Контакты',
+			href: '/contact',
+			isShow: true
+		},
+        {
+			title: 'temp',
+			href: '/temp',
+			isShow: true
+		}
+	]
 </script>
-
-  <style>
+<style>
     .container {
-      max-width: 1400px;
-      margin: 50px auto;
+        max-width: 1400px;
+        margin: 50px auto;
     }
-  </style>
+</style>
