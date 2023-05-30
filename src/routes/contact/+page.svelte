7fbec8977f45d70a80030dca7109c749bbe2577e
Artem <artemtusaliev22@gmail.com>
2023-05-30 16:13:52 +0500

    w

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,5 +1,13 @@
 <h1>Контакты</h1>
 
+<style>	body {
+    background-position: left top;
+    font-family: "Inter", sans-serif;
+    margin: 0;
+    background-repeat: no-repeat;
+  }
+  </style>
+
 <ul><li>Gmail: artemtusaliev22@gmail.com</li>
     <li>Телефон: +79821683951</li>
     <li>Вконтакте: https://vk.com/artem</li>
