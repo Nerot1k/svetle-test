e14cb79807694bcbf96b540adfef0db5c72f66f1
Artem <artemtusaliev22@gmail.com>
2023-05-30 18:16:40 +0500

    2

---
+++ src/routes/project/apps/3/+page.svelte
@@ -0,0 +1,46 @@
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>МузВикторина</title>
+</head>
+
+<body>
+
+
+    <div class="quiz-container" id="quiz">
+
+        <div class="quiz-header">
+            <h1 id="title">МузВикторина <img src="f1.jpg"></h1>
+            <h2 id="question">Вопрос1</h2>
+            <ul>
+                <li>
+                    <input type="radio" name="answer" id="a" class="answer">
+                    <label for="a" id="a_text">*</label>
+                </li>
+
+                <li>
+                    <input type="radio" name="answer" id="b" class="answer">
+                    <label for="b" id="b_text">*</label>
+                </li>
+
+                <li>
+                    <input type="radio" name="answer" id="c" class="answer">
+                    <label for="c" id="c_text">*</label>
+                </li>
+
+                <li>
+                    <input type="radio" name="answer" id="d" class="answer">
+                    <label for="d" id="d_text">*</label>
+                </li>
+
+            </ul>
+        </div>
+
+        <button id="submit">Следующий вопрос</button>
+
+    </div>
+
+
+    <script src="script.js"></script>
+
+</body>
