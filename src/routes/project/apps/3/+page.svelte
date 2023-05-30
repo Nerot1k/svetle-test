03086e28a10a6c903fd56c8f50d0197006a65f35
Artem <artemtusaliev22@gmail.com>
2023-05-30 18:41:53 +0500

    2

--- src/routes/project/apps/3/+page.svelte
+++ src/routes/project/apps/3/+page.svelte
@@ -1,209 +1,91 @@
-   <div class="quiz-container" id="quiz">
+<h1>Учет расходов</h1>
 
-        <div class="quiz-header">
-            <h1 id="title">МузВикторина</h1>
-            <h2 id="question">Вопрос1</h2>
-            <ul>
-                <li>
-                    <input type="radio" name="answer" id="a" class="answer">
-                    <label for="a" id="a_text">*</label>
-                </li>
+<form on:submit|preventDefault={addExpense}>
+ <label for="name">Название:</label>
+ <input type="text" id="name" bind:value={newName} />
+ <label for="amount">Сумма:</label>
+ <input type="text" id="amount" bind:value={newAmount} />
+ <button type="submit">Добавить</button>
+</form>
 
-                <li>
-                    <input type="radio" name="answer" id="b" class="answer">
-                    <label for="b" id="b_text">*</label>
-                </li>
-
-                <li>
-                    <input type="radio" name="answer" id="c" class="answer">
-                    <label for="c" id="c_text">*</label>
-                </li>
-
-                <li>
-                    <input type="radio" name="answer" id="d" class="answer">
-                    <label for="d" id="d_text">*</label>
-                </li>
-
-            </ul>
-        </div>
-
-        <button id="submit">Следующий вопрос</button>
-
-    </div>
+<table>
+ <thead>
+  <tr>
+   <th>Название</th>
+   <th>Сумма</th>
+   <th>Действия</th>
+  </tr>
+ </thead>
+ <tbody>
+  {#each expenses as expense, i}
+   <tr>
+    <td><div>{expense.name}</div> </td>
+    <td><div>{expense.amount}</div> </td>
+    <td><button on:click={() => deleteExpense(i)}>Удалить</button></td>
+   </tr>
+  {/each}
+ </tbody>
+</table>
 
 <script>
+ let expenses = [];
 
-const quizData = [
-    {
-        question: "Какой из предложенных вариантов не является струнно-щепковым музыкальным инструментом?",
-        a: "Барабан",
-        b: "Укулеле",
-        c: "Арфа",
-        d: "Электрогитара",
-        correct: "a"
- },
-    {
-        question: "Чем отличается Акустическая гитара от Классической?",
-        a: "Барабан, то есть корпус самой гитары",
-        b: "Гриф",
-        c: "Форма колков и то, как они выглядят",
-        d: "Длиной струн",
-        correct: "c"
-    }, {
-        question: "Кто из этих исполнителей прославился игрой на электрогитаре?",
-        a: "Высоцкий",
-        b: "В. Цой",
-        c: "Пол Маккартни",
-        d: "Курт Кобейн",
-        correct: "d",
-    }, {
-        question: "Что такое мензура в гитаре?",
-        a: "Длина струн",
-        b: "Расстояние от верхнего до нижнего порожка",
-        c: "То, за что крепятся струны",
-        d: "Приём, для смены звука",
-        correct: "b"
-    }, {
-        question: "Из каких материалов не сущесвтует Медиаторов?",
-        a: "Из кости",
-        b: "Из глянца",
-        c: "Из пластика",
-        d: "Из дерева",
-        correct: "b",
-    }
-]
-const quiz = document.getElementById('quiz')
-const answerEls = document.querySelectorAll('.answer')
-const questionEl = document.getElementById('question')
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
-const submitBtn = document.getElementById('submit')
+ let newName = '';
+ let newAmount = '';
 
+ function addExpense() {
+  const obj = {
+   name: newName,
+   amount: parseInt(newAmount),
+  };
+  expenses.push(obj);
+  expenses = expenses;
+  newName = '';
+  newAmount = '';
+ }
 
-let currentQuiz = 0
-let score = 0
-
-loadQuiz()
-
-function loadQuiz() {
-
-    deselectAnswers()
-
-    const currentQuizData = quizData[currentQuiz]
-
-    questionEl.innerText = currentQuizData.question
-    a_text.innerText = currentQuizData.a
-    b_text.innerText = currentQuizData.b
-    c_text.innerText = currentQuizData.c
-    d_text.innerText = currentQuizData.d
-}
-
-function deselectAnswers() {
-    answerEls.forEach(answerEl => answerEl.checked = false)
-}
-
-function getSelected() {
-    let answer
-    answerEls.forEach(answerEl => {
-        if (answerEl.checked) {
-            answer = answerEl.id
-        }
-    })
-    return answer
-}
-
-
-submitBtn.addEventListener('click', () => {
-    const answer = getSelected()
-    if (answer) {
-        if (answer === quizData[currentQuiz].correct) {
-            score++
-        }
-
-        currentQuiz++
-
-        if (currentQuiz < quizData.length) {
-            loadQuiz()
-        } else {
-            quiz.innerHTML = `
-           <h2>Вы ответили верно на    ${score}  из  ${quizData.length} заданных вопросов</h2>
-
-           <button onclick="location.reload()">Пройти заново</button>
-           `
-        }
-    }
-})
-
-
+ function deleteExpense(index) {
+  expenses.splice(index, 1);
+  expenses = expenses;
+ }
 </script>
 
-    <style>
-        
-        * {
-    box-sizing: border-box;
-}
-.quiz-container {
-    background-color: #F1F1F1;
-    border-radius: 10px;
-    box-shadow: 0 0 10px 2px rgba(100, 100, 100, 0.1);
-    width: 500px;
-    overflow: hidden;
-}
-
-.quiz-header {
-    padding: 40px;
-}
-
-h1 {
-    padding: 10px;
-    text-align: center;
-    margin: 0;
-    color: #4682B4;
-    padding: 60px;
-}
-
-h2 {
-    padding: 1rem;
-    text-align: center;
-    margin: 0;
-
-}
-
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-ul li {
-    font-size: 18px;
-    margin: 10px 0;
-}
-
-ul li label {
-    cursor: pointer;
-}
-
-button {
-    background-color: #76C835;
-    color: #fff;
-    border: none;
-    display: block;
-    width: 100%;
-    cursor: pointer;
-    font-size: 21px;
-    font-family: inherit;
-    padding: 23px;
-}
-
-button:hover {
-    background-color: #039E50;
-}
-
-button:focus {
-    outline: none;
-    background-color: #03654D;
-}
-
-        </style>
+<style>
+ label {
+  display: block;
+  margin-bottom: 10px;
+ }
+ input[type='text'] {
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-right: 10px;
+  width: 150px;
+ }
+ table {
+  margin-top: 20px;
+  border-collapse: collapse;
+  width: 100%;
+ }
+ th,
+ td {
+  border: 1px solid #ccc;
+  padding: 8px;
+  text-align: left;
+ }
+ th {
+  background-color: #eee;
+ }
+ button {
+  padding: 5px 10px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+ }
+ h1 {
+  font-size: 24px;
+  margin-bottom: 20px;
+ }
+</style>
