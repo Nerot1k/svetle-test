30acc65c6e55368ae43abfef49d572cdd38cf138
Artem <artemtusaliev22@gmail.com>
2023-05-31 11:26:07 +0500

    23

--- src/routes/project/apps/4/+page.svelte
+++ src/routes/project/apps/4/+page.svelte
@@ -1,3 +1,3 @@
-<a href="https://nerot1k.github.io/deer">Ссылка на сайт</a>
+<strong><u><a href="https://nerot1k.github.io/deer">Ссылка на сайт</a></u></strong>
 <br>
 <img class="photo svelte-192ixv5" src="/img/2023-05-31_11-15-05.png" alt="photo" width=50%>
